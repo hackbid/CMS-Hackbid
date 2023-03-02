a0efc4e392d35dab3e1948bdcb2ef32792a27ba2
dwikiop <dwikiokvianp@gmail.com>
2023-03-03 01:18:08 +0700

    feat - add sidebar to main dashboard

--- hackbid-admin/src/App.jsx
+++ hackbid-admin/src/App.jsx
@@ -1,12 +1,14 @@
 import { useState } from "react";
 import Login from "./pages/Login/Login.jsx";
+import AdminDashboard from "./pages/AdminDashboard/AdminDashboard.jsx";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <Login />
+      {/*<Login />*/}
+      <AdminDashboard />
     </>
   );
 }
